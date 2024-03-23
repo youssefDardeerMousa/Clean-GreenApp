d3ea0dea7126e611d6b967a858b14936c5ca3782
Youssef Dardeer Mousa <Youssef.Dardeer.Mousa@gmail.com>
2024-03-23 15:03:30 +0200

    update product

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -43,7 +43,7 @@ export const addProduct = CatchError(async (req, res, next) => {
 
   // upload default image 
   const { secure_url, public_id } = await cloudinary.uploader.upload(
-    req.files.defaultImage[0].path,
+    req.files.defaultImage.path,
     { folder: `${process.env.foldercloudnairy}/products/${cloudFolder}` }
   );
 
